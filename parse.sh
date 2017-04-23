24f9072e69b2f15908b8f104b2e4ba10ff84fffd
MoeBrowne <oliver.browne@uplyme.com>
2017-04-23 20:50:05 +0100

    Skipped lines for localhost

--- parse.sh
+++ parse.sh
@@ -7,7 +7,14 @@ while read line; do
 	[[ $line =~ $regexLine ]]
 
 	if [[ ${BASH_REMATCH[0]} != "" ]]; then
-		echo "zone \"${BASH_REMATCH[1]}\" { type master;  file \"/etc/named/blackhole\"; };"
+
+		FQDN="${BASH_REMATCH[1]}"
+
+		if [[ "$FQDN" = "localhost" ]]; then
+			continue;
+		fi
+
+		echo "zone \"$FQDN\" { type master;  file \"/etc/named/blackhole\"; };"
 	fi
 
 done < /dev/stdin
