bba009be0e2f52c8fc57e57555afd373046e8b29
MoeBrowne <oliver.browne@uplyme.com>
2017-04-25 21:50:46 +0100

    Added some rudimentary ETag based caching

--- download.sh
+++ download.sh
@@ -1,11 +1,29 @@
 #!/usr/bin/env bash
 
+regexETagHeader='ETag: "([^"]+)"'
+
 while read source; do
-	echo "Downloading from $source" > /dev/stderr
-	sourceList=$(curl -s "$source")
+	echo "Getting hosts from $source" > /dev/stderr
+
+	headers=$(curl -sI "$source")
+
+	ETag=""
+	if [[ $headers =~ $regexETagHeader ]]; then
+		ETag="${BASH_REMATCH[1]}"
+	fi
+
+	if [[ -f "./cache/${ETag}" ]]; then
+		echo " - Using cache" > /dev/stderr
+		sourceList=$(cat "./cache/${ETag}")
+	else
+		echo " - Downloading..." > /dev/stderr
+		sourceList=$(curl -s "$source")
+		echo "$sourceList" > "./cache/${ETag}"
+	fi
+
 	sourceCount=$(wc -l <<< "${sourceList}")
 
-	echo " Found $sourceCount hosts" > /dev/stderr
+	echo " - Got $sourceCount hosts" > /dev/stderr
 
 	echo "$sourceList"
 
