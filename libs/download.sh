a84f60a1dd1c1c0ac3c1f342c30cbc04c4cadec4
MoeBrowne <oliver.browne@uplyme.com>
2017-08-11 21:42:47 +0100

    Added a check to ensure that the request responds with 200

--- libs/download.sh
+++ libs/download.sh
@@ -10,11 +10,19 @@ if [ ! -d "$DIR_CACHE" ]; then
 fi
 
 regexETagHeader='ETag: "([^"]+)"'
+regexCodeHeader='HTTP/[1-2]\.[1-2] ([0-9]+) ([a-zA-Z0-9 ]+)'
 
 while read source; do
 	echo "Getting hosts from $source" > /dev/stderr
 
 	headers=$(curl -sI "$source")
+
+	# Check that we got a 200 response
+	[[ $headers =~ $regexCodeHeader ]]
+	if [[ "${BASH_REMATCH[1]}" != "200" ]]; then
+	    echo " - Error: ${BASH_REMATCH[2]}"
+	    continue
+	fi
 
 	ETag=""
 	if [[ $headers =~ $regexETagHeader ]]; then
